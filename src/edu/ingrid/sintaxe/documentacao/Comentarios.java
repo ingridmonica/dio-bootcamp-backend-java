57b46d2aeea582306fee5c48d6fdc50c545ae6ec
ingridmonica <ingridmonica987@gmail.com>
2024-06-16 00:13:48 -0300

    códigos sobre a documentação do java

---
+++ src/edu/ingrid/sintaxe/documentacao/Comentarios.java
@@ -0,0 +1,23 @@
+package src.edu.ingrid.sintaxe.documentacao;
+
+public class Comentarios {
+    public static void main(String [] args){
+        // Olá, eu sou um comentário em uma única linha
+
+        /* Olá,
+         * Eu sou um comentario
+         * que posso ser mais detalhadod
+         * quando necessário
+         */
+    }
+    /** DOCUMENTAÇÃO
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nível de documentação.
+     * Que incrível !!!
+     */
+    public void metodo(){
+
+    }
+}
